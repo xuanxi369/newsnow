99fa64f9d4141206ccb6550d73f05af414aa44e8
xuanxi369 <151494659+xuanxi369@users.noreply.github.com>
2025-07-27 08:04:14 +0000

    Update chongbuluo.ts

--- server/sources/chongbuluo.ts
+++ server/sources/chongbuluo.ts
@@ -1,32 +1,32 @@
-import type { NewsItem } from "@shared/types"
-import * as cheerio from "cheerio"
+// import type { NewsItem } from "@shared/types"
+// import * as cheerio from "cheerio"
 
-const hot = defineSource(async () => {
-  const baseUrl = "https://www.chongbuluo.com/"
-  const html: string = await myFetch(`${baseUrl}forum.php?mod=guide&view=hot`)
-  const $ = cheerio.load(html)
-  const news: NewsItem[] = []
+// const hot = defineSource(async () => {
+//   const baseUrl = "https://www.chongbuluo.com/"
+//   const html: string = await myFetch(`${baseUrl}forum.php?mod=guide&view=hot`)
+//   const $ = cheerio.load(html)
+//   const news: NewsItem[] = []
 
-  $(".bmw table tr").each((_, elem) => {
-    const xst = $(elem).find(".common .xst").text()
-    const url = $(elem).find(".common a").attr("href")
-    news.push({
-      id: baseUrl + url,
-      url: baseUrl + url,
-      title: xst,
-      extra: {
-        hover: xst,
-      },
-    })
-  })
+//   $(".bmw table tr").each((_, elem) => {
+//     const xst = $(elem).find(".common .xst").text()
+//     const url = $(elem).find(".common a").attr("href")
+//     news.push({
+//       id: baseUrl + url,
+//       url: baseUrl + url,
+//       title: xst,
+//       extra: {
+//         hover: xst,
+//       },
+//     })
+//   })
 
-  return news
-})
+//   return news
+// })
 
-const latest = defineRSSSource("https://www.chongbuluo.com/forum.php?mod=rss&view=newthread")
+// const latest = defineRSSSource("https://www.chongbuluo.com/forum.php?mod=rss&view=newthread")
 
-export default defineSource({
-  "chongbuluo": hot,
-  "chongbuluo-hot": hot,
-  "chongbuluo-latest": latest,
-})
+// export default defineSource({
+//   "chongbuluo": hot,
+//   "chongbuluo-hot": hot,
+//   "chongbuluo-latest": latest,
+// })
